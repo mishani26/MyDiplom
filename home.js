fd4aeff10138caa0044773ee0c19f399d5c596ba
Micheal <misha.badil@gmail.com>
2017-05-17 23:33:05 +0300

    added crypto for project

--- home.js
+++ home.js
@@ -2,6 +2,23 @@ var app = require('electron').remote;
 var dialog = app.dialog;
 var fs = require('fs');
 var enigma = require('enigma-js')
+var crypt = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'd6F3Efeq';
+
+  function encrypt(text){
+  var cipher = crypt.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+ 
+function decrypt(text){
+  var decipher = crypt.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
 
 var default_settings = {
   rotors: [{
@@ -30,7 +47,7 @@ var default_settings = {
   spacing: 4
 }
 
-document.getElementById('create-file').addEventListener('click', function() {
+document.getElementById('create-file').onclick=()=> {
   dialog.showSaveDialog((fileName) => {
     // fileNames is an array that contains all the selected
     if (fileName === undefined) {
@@ -41,7 +58,8 @@ document.getElementById('create-file').addEventListener('click', function() {
       saveFile(fileName);
     }
   });
-}, false);
+};
+
 document.getElementById('open-file').onclick=()=>{
   dialog.showOpenDialog((filenames)=>{
     if (filenames === undefined) {
@@ -54,26 +72,26 @@ document.getElementById('open-file').onclick=()=>{
 }
 
 function saveFile(fileName) {
-
   enigma.load(JSON.parse(JSON.stringify(default_settings)))
   let content=enigma.process(document.getElementById('inputTextArea').value.toUpperCase());
   default_settings.spacing = 0
   enigma.load(JSON.parse(JSON.stringify(default_settings)))
   let decrypted_message = enigma.process(content.replace(/ /g,''))
   alert(
-    content+"("+decrypted_message+")"
+    encrypt(content)+"("+decrypt(encrypt(decrypted_message))+")"
   )
-  fs.writeFile(fileName,content,(err)=>{
+  fs.writeFile(fileName,encrypt(content),(err)=>{
     if (err) elert(err);
     elert("file has been saved");
   });
 
 }
+
 function readFile(filepath) {
   fs.readFile(filepath,"utf-8", (err, data) => {
   if (err) throw err;
   enigma.load(JSON.parse(JSON.stringify(default_settings)))
-  let decrypted_message = enigma.process(data);
+  let decrypted_message = enigma.process(decrypt(data));
   alert(decrypted_message);
   document.getElementById('inputTextArea').value=decrypted_message;
 });
